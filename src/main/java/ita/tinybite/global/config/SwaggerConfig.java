2a42bdd55ca0d76603712b69cd4780d031b865cc
Wonjae Lim <yyytir777@gmail.com>
2025-12-07 00:44:52 +0900

    fix : swagger 오류 해결

--- src/main/java/ita/tinybite/global/config/SwaggerConfig.java
+++ src/main/java/ita/tinybite/global/config/SwaggerConfig.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +18,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("https://growinserver.shop")))
                 .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
                 .components(
                         new Components()
