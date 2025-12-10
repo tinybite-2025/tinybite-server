00db9fbf7cb2a1aadcda61c776254a5849c8a40c
marshmallowing <yuin1111801@naver.com>
2025-12-10 19:32:20 +0900

    feat: 알림 요청 Dto

---
+++ src/main/java/ita/tinybite/domain/fcm/dto/request/NotificationMulticastRequest.java
@@ -0,0 +1,53 @@
+package ita.tinybite.domain.fcm.dto.request;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.firebase.internal.NonNull;
+import com.google.firebase.messaging.MulticastMessage;
+import com.google.firebase.messaging.Notification;
+
+import lombok.Builder;
+
+//@Builder(access = PRIVATE)
+@Builder
+public record NotificationMulticastRequest(
+	@NonNull List<String> tokens,
+	String title,
+	String body,
+	Map<String, String> data
+) implements NotificationRequest {
+
+	public static NotificationMulticastRequest of(List<String> tokens, String title, String body, Map<String, String> data) {
+		return NotificationMulticastRequest.builder()
+			.tokens(tokens)
+			.title(title)
+			.body(body)
+			.data(data)
+			.build();
+	}
+
+	public MulticastMessage.Builder buildSendMessage() {
+		MulticastMessage.Builder builder = MulticastMessage.builder()
+			.setNotification(toNotification())
+			.addAllTokens(tokens);
+
+		if (this.data != null && !this.data.isEmpty()) {
+			builder.putAllData(this.data);
+		}
+
+		return builder;
+	}
+
+	public Notification toNotification() {
+		return Notification.builder()
+			.setTitle(title)
+			.setBody(body)
+			.build();
+	}
+
+	@Override
+	public Map<String, String> data() {
+		return this.data;
+	}
+}
---
+++ src/main/java/ita/tinybite/domain/fcm/dto/request/NotificationSingleRequest.java
@@ -0,0 +1,52 @@
+package ita.tinybite.domain.fcm.dto.request;
+
+import java.util.Map;
+
+import com.google.firebase.internal.NonNull;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
+import lombok.Builder;
+
+//@Builder(access = PRIVATE)
+@Builder
+public record NotificationSingleRequest(
+	@NonNull String token,
+	String title,
+	String body,
+	Map<String, String> data
+) implements NotificationRequest {
+
+	public static NotificationSingleRequest of(String token, String title, String body, Map<String, String> data) {
+		return NotificationSingleRequest.builder()
+			.token(token)
+			.title(title)
+			.body(body)
+			.data(data)
+			.build();
+	}
+
+	public Message.Builder buildMessage() {
+		Message.Builder builder = Message.builder()
+			.setToken(token)
+			.setNotification(toNotification());
+
+		if (this.data != null && !this.data.isEmpty()) {
+			builder.putAllData(this.data);
+		}
+
+		return builder;
+	}
+
+	public Notification toNotification() {
+		return Notification.builder()
+			.setTitle(title)
+			.setBody(body)
+			.build();
+	}
+
+	@Override
+	public Map<String, String> data() {
+		return this.data;
+	}
+}
