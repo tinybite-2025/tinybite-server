0cd921fca00ceb368e94560ed24c42d9e31fda7c
Wonjae Lim <yyytir777@gmail.com>
2025-12-11 23:36:15 +0900

    fix : user hard delete

--- src/main/java/ita/tinybite/domain/auth/service/AuthService.java
+++ src/main/java/ita/tinybite/domain/auth/service/AuthService.java
@@ -293,6 +293,7 @@ public class AuthService {
     @Transactional
     public void logout(Long userId) {
         refreshTokenRepository.deleteByUserId(userId);
+        userRepository.deleteById(userId);
         log.info("로그아웃 - User ID: {}", userId);
     }
 
