ed3a399c93ab9d47bd8548913ad72214ae8dfcdd
Donghun Won <wonhun1225@gmail.com>
2026-01-02 21:52:17 +0900

    hotfix : 참여중 파티 조회 반환 형식 통일

--- src/main/java/ita/tinybite/domain/user/service/UserService.java
+++ src/main/java/ita/tinybite/domain/user/service/UserService.java
@@ -1,13 +1,13 @@
 package ita.tinybite.domain.user.service;
 
 import ita.tinybite.domain.auth.service.SecurityProvider;
+import ita.tinybite.domain.party.dto.response.PartyCardResponse;
 import ita.tinybite.domain.party.entity.Party;
 import ita.tinybite.domain.party.entity.PartyParticipant;
 import ita.tinybite.domain.party.enums.ParticipantStatus;
 import ita.tinybite.domain.party.enums.PartyStatus;
 import ita.tinybite.domain.party.repository.PartyParticipantRepository;
 import ita.tinybite.domain.user.dto.req.UpdateUserReqDto;
-import ita.tinybite.domain.user.dto.res.PartyResponse;
 import ita.tinybite.domain.user.dto.res.UserResDto;
 import ita.tinybite.domain.user.entity.User;
 import ita.tinybite.domain.user.repository.UserRepository;
@@ -67,7 +67,7 @@ public class UserService {
             throw BusinessException.of(AuthErrorCode.DUPLICATED_NICKNAME);
     }
 
-    public List<PartyResponse> getActiveParties(Long userId) {
+    public List<PartyCardResponse> getActiveParties(Long userId) {
         List<PartyParticipant> participants = participantRepository
                 .findActivePartiesByUserId(
                         userId,
@@ -81,7 +81,7 @@ public class UserService {
                     int currentParticipants = participantRepository
                             .countByPartyIdAndStatus(party.getId(), ParticipantStatus.APPROVED);
                     boolean isHost = party.getHost().getUserId().equals(userId);
-                    return PartyResponse.from(party, currentParticipants, isHost,pp.getStatus());
+                    return PartyCardResponse.from(party, currentParticipants, isHost,pp.getStatus());
                 })
                 .collect(Collectors.toList());
     }
