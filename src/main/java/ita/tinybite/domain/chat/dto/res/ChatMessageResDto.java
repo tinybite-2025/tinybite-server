852ecc1b428a4ac7f0e2b8c4a6eb2cac8b35567d
Wonjae Lim <yyytir777@gmail.com>
2026-01-10 16:53:05 +0900

    hotfix : date 메시지 필드 수정

--- src/main/java/ita/tinybite/domain/chat/dto/res/ChatMessageResDto.java
+++ src/main/java/ita/tinybite/domain/chat/dto/res/ChatMessageResDto.java
@@ -1,5 +1,6 @@
 package ita.tinybite.domain.chat.dto.res;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import ita.tinybite.domain.chat.entity.ChatMessage;
 import ita.tinybite.domain.chat.enums.MessageType;
 import lombok.Builder;
@@ -7,6 +8,7 @@ import lombok.Builder;
 import java.time.LocalDateTime;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ChatMessageResDto(
         Long messageId,
         MessageType messageType,
@@ -22,16 +24,28 @@ public record ChatMessageResDto(
         String systemMessage
 ) {
     public static ChatMessageResDto of(ChatMessage chatMessage, Long senderId) {
-        return ChatMessageResDto.builder()
-                .messageId(chatMessage.getId())
-                .messageType(chatMessage.getMessageType())
-                .createdAt(chatMessage.getCreatedAt())
-                .senderId(chatMessage.getSenderId())
-                .isMine(senderId.equals(chatMessage.getSenderId()))
-                .nickname(chatMessage.getSenderName())
-                .text(chatMessage.getText())
-                .imageUrl(chatMessage.getImageUrl())
-                .systemMessage(chatMessage.getSystemMessage())
-                .build();
+        switch (chatMessage.getMessageType()) {
+            case DATE -> {
+                return ChatMessageResDto.builder()
+                        .messageId(chatMessage.getId())
+                        .messageType(chatMessage.getMessageType())
+                        .createdAt(chatMessage.getCreatedAt())
+                        .systemMessage(chatMessage.getSystemMessage())
+                        .build();
+            }
+            default -> {
+                return ChatMessageResDto.builder()
+                        .messageId(chatMessage.getId())
+                        .messageType(chatMessage.getMessageType())
+                        .createdAt(chatMessage.getCreatedAt())
+                        .senderId(chatMessage.getSenderId())
+                        .isMine(senderId.equals(chatMessage.getSenderId()))
+                        .nickname(chatMessage.getSenderName())
+                        .text(chatMessage.getText())
+                        .imageUrl(chatMessage.getImageUrl())
+                        .systemMessage(chatMessage.getSystemMessage())
+                        .build();
+            }
+        }
     }
 }
