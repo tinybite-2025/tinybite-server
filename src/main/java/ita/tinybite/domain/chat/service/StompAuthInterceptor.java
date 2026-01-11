193fc93458b961d56d957352d7f64f9f3a6c642e
Wonjae Lim <yyytir777@gmail.com>
2026-01-11 19:38:20 +0900

    hotfix : HandShakeInterceptor 변경

--- src/main/java/ita/tinybite/domain/chat/service/StompAuthInterceptor.java
+++ src/main/java/ita/tinybite/domain/chat/service/StompAuthInterceptor.java
@@ -4,6 +4,8 @@ import ita.tinybite.domain.auth.entity.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -11,6 +13,10 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
 
 /**
  * http가 아닌, 웹소켓에서 인증을 위한 인터셉터 (filter는 사용 안됨)
@@ -18,30 +24,34 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class StompAuthInterceptor implements ChannelInterceptor {
+public class StompAuthInterceptor implements HandshakeInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public Message<?> preSend(@NotNull Message<?> message, @NotNull MessageChannel channel) {
-        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        log.info("StompAuthInterceptor preSend");
-        log.info(accessor.getCommand().toString());
-        if(StompCommand.CONNECT.equals(accessor.getCommand())) {
-            String authHeader = accessor.getFirstNativeHeader("Authorization");
-            log.info("StompAuthInterceptor authHeader: {}", authHeader);
+    public boolean beforeHandshake(
+            ServerHttpRequest request,
+            ServerHttpResponse response,
+            WebSocketHandler wsHandler,
+            Map<String, Object> attributes
+    ) {
+        String authHeader = request.getHeaders().getFirst("Authorization");
+        log.info("StompAuthInterceptor authHeader: {}", authHeader);
 
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                throw new IllegalArgumentException("Missing or invalid Authorization header");
-            }
-
-            String token = authHeader.substring(7);
-            log.info("StompAuthInterceptor preSend token: {}", token);
-            jwtTokenProvider.validateToken(token);
-            Long userId = jwtTokenProvider.getUserId(token);
-            log.info("StompAuthInterceptor preSend userId: {}", userId);
-            accessor.getSessionAttributes().put("userId", userId);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new IllegalArgumentException("Missing or invalid Authorization header");
         }
-        return message;
+
+        String token = authHeader.substring(7);
+        log.info("StompAuthInterceptor preSend token: {}", token);
+        jwtTokenProvider.validateToken(token);
+        Long userId = jwtTokenProvider.getUserId(token);
+        log.info("StompAuthInterceptor preSend userId: {}", userId);
+        attributes.put("userId", userId);
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
     }
 }
