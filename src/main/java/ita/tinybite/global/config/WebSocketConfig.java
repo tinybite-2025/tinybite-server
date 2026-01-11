55bbc77a50ca38251b665d8795e4565b316c3c56
Wonjae Lim <yyytir777@gmail.com>
2026-01-11 21:32:12 +0900

    hotfix : add DefaultHandShakeHandler

--- src/main/java/ita/tinybite/global/config/WebSocketConfig.java
+++ src/main/java/ita/tinybite/global/config/WebSocketConfig.java
@@ -9,6 +9,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Slf4j
 @Configuration
@@ -27,7 +28,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .setAllowedOriginPatterns("*");
+                .setAllowedOriginPatterns("*")
+                .setHandshakeHandler(new DefaultHandshakeHandler())
+        ;
     }
 
     @Override
